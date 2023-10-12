533cd3cd216c285528506444101c961a88510d57
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-10-12 21:47:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,19 +8,23 @@ const heading = document.querySelector(".order-heading");
 const orderId = document.querySelector("#orderid");
 const searchButton = document.querySelector("button");
 const orderSearch = document.querySelector(".order-search")
+const workID = document.querySelector("#workid")
 
 
 
 searchButton.addEventListener('click', () => {
     console.log(orderId.value)
     if (workIDs.includes(parseInt(orderId.value))){
-        
+        afterResult.style.background = "url(./images/order-received.png)";
         beforeResult.style.display = "none";
+        workID.innerHTML += parseInt(orderId.value)
         afterResult.style.display = "flex";
     }
     else{
+        beforeResult.style.background = "red";
         orderSearch.style.display = "none"
-        heading.innerHTML = "<h1>You may entered a wrong id number please check the order id and it has no spaces</h1>"
+        heading.innerHTML = "<h2>You may entered a wrong id number please check the order id and it has no spaces</h2>"
+        
     }
 })
 
